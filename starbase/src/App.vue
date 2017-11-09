0ccc1e2c189d4dca973437f0c44ff18d62e034bb
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2017-11-09 13:48:06 -0200

    29 - Finalizing Character Behavior

--- starbase/src/App.vue
+++ starbase/src/App.vue
@@ -1,16 +1,38 @@
 <template>
     <div id="app">
         <h3>{{title}}</h3>
+        <div class="col-md-12">
+            <Character 
+                v-for="(id, index) in initial_ids"
+                v-bind:id="id"
+                key="index"
+            />
+        </div>
     </div>
 </template>
 
 <script>
+import Character from './components/Character.vue'
+
 export default {
     name: 'app',
     data() {
         return {
-            title:'Generate your team'
+            title:'Generate your team',
+            initial_ids:[1, 13, 14]
         }
+    },
+    methods: {
+        fetchCharacter(){
+            fetch('https://swapi.co/api/people/', {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => console.log(json))
+        }
+    },
+    components: {
+        Character
     }
 }
 </script>
