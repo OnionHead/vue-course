f79b2d527582b2794cbfd6fe9d979af10cfcfd5b
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2017-11-17 16:38:24 -0200

    48 - Coding Break | The Vuex Recipe

--- jokester/src/components/App.vue
+++ jokester/src/components/App.vue
@@ -1,5 +1,19 @@
 <template>
 <div id="app">
-    Got Jokes? {{$store.state}}
+    <h3>Got Jokes?</h3>
+    <button v-on:click="initJokes" class="btn btn-primary">Get 10 random jokes</button>
+    <div class="">
+        {{$store.state}}
+    </div>
 </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+    methods: mapActions([
+        'initJokes'
+    ])
+}
+</script>
