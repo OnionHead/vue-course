381926fb97b044b1de3a8dd52b81755c3ada36b8
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2017-11-28 17:37:11 -0200

    60 - Signing Out

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -1,7 +1,22 @@
 <template>
 <div class="">
+    <h3>Events Dashboard</h3>
+    <button class="btn btn-danger btn-sm signtout-btn" @click="signOut">Sign Out</button>
     Dashboard Component
 
     {{$store.state}}
 </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp'
+
+export default {
+    methods:{
+        signOut(){
+            this.$store.dispatch('signOut')
+            firebaseApp.auth().signOut();
+        }
+    }
+}
+</script>
