924d08ac6584bdc9b2e4ccbcc23043e5b32cd75a
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2017-11-28 17:48:44 -0200

    61 - Developing the Sign Up Component

--- applications/turnout/src/components/Signup.vue
+++ applications/turnout/src/components/Signup.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="from-inline">
-    <h3>Sign In</h3>
+    <h3>Sign Up</h3>
     <div class="form-group">
         <input
             type="text"
@@ -16,11 +16,11 @@
             v-model="password"
         />
         <br/>
-        <button class="btn btn-primary" @click="signIn">Sign In</button>
+        <button class="btn btn-primary" @click="signUp">Sign Up</button>
     </div>
     <br/>
     <p>{{error.message}}</p>
-    <router-link to="/signup">Not a user? Sign up</router-link>
+    <router-link to="/signin">Already a user? Sign In</router-link>
 </div>
 </template>
 
@@ -28,7 +28,7 @@
 import { firebaseApp } from '../firebaseApp'
 
 export default {
-    data(){
+    data() {
         return {
             email: '',
             password: '',
@@ -37,9 +37,9 @@ export default {
             }
         }
     },
-    methods:{
-        signIn() {
-            firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+    methods: {
+        signUp() {
+            firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
             .catch(error => {
                 this.error = error
             })
