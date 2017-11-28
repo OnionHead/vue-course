2241fc4114c390f89155ccb4126d7a253a4a3404
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2017-11-28 18:07:18 -0200

    63 - Submitting Events

---
+++ applications/turnout/src/components/AddEvent.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="">
+    <h4>
+        Add an Event 
+        <input type="checkbox" v-model="showForm"/>
+    </h4>
+    <div class="form" v-show="showForm">
+        <div class="form-group">
+            <label>Title</label>
+            <input type="text" class="form-control" v-model="event.title" />
+        </div>
+        <div class="form-group">
+            <label>description</label>
+            <input type="text" class="form-control" v-model="event.description" />
+        </div>
+        <div class="form-group">
+            <label>Date</label>
+            <input type="date" class="form-control" v-model="event.date" />
+        </div>
+        <div class="form-group">
+            <label>Location</label>
+            <input type="text" class="form-control" v-model="event.location" />
+        </div>
+        <div class="form-group">
+            <label>E-mail</label>
+            <input type="email" class="form-control" v-model="event.email" />
+        </div>
+        <button class="btn-sm btn-primary" @click="addEvent">Submit</button>
+    </div>
+</div>
+</template>
+
+<script>
+import { eventsRef } from '../firebaseApp'
+
+export default {
+    data() {
+        return {
+            event: {
+                title: '',
+                description: '',
+                date: '',
+                location: '',
+                email: ''
+            },
+            showForm: false
+        }
+    },
+    methods: {
+        addEvent() {
+            this.event.email = this.$store.state.user.email;
+
+            eventsRef.push(this.event)
+        }
+    }
+}
+</script>
